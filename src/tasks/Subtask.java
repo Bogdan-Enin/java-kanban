028dbcf45c43aaba7258092e326fae72fe432d94
Bogdan Enin <bogdanjenin@yandex.ru>
2025-01-19 12:56:26 +0200

    Переместил класс Subtask в пакет tasks, методы используют идентификаторы (subtaskIds) вместо хранения объектов напрямую.

---
+++ src/tasks/Subtask.java
@@ -0,0 +1,23 @@
+package tasks;
+
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String name, String description, int id, TaskStatus status, int epicId) {
+        super(name, description, id, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String getType() {
+        return "Subtask";
+    }
+}
